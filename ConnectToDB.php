740ca32d6c8056ce58938b7bc1c86ab1dea5112a
nguyen tri <rinthu1997@gmail.com>
2019-06-11 16:34:59 +0700

    Update ConnectToDB.php
    change PostgreSQL path

--- ConnectToDB.php
+++ ConnectToDB.php
@@ -20,7 +20,7 @@ if (empty(getenv("DATABASE_URL"))){
      echo getenv("dbname");
    $db = parse_url(getenv("DATABASE_URL"));
    $pdo = new PDO("pgsql:" . sprintf(
-        "host=ec2-174-129-240-67.compute-1.amazonaws.com;port=5432;user=wrflrxtavasvqh;password=fbfef36049fbd28f1200e3a775a389e014838e86522765e67782f9cf7a3f516b;dbname=d3mmhribgmc6bf",
+        "host=ec2-23-21-129-125.compute-1.amazonaws.com;port=5432;user=lrqgdgzdowigqc;password=8652e83044024491ba444c1d161044f803b99c9101bd7d129e8a6a6db78b5bea;dbname=deh478l9lii3uq",
         $db["host"],
         $db["port"],
         $db["user"],
