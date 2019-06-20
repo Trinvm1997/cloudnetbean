3ee71c8f3a2fd4e41b892ccffdb311ca28510ae1
nguyen tri <rinthu1997@gmail.com>
2019-06-20 23:30:42 +0700

    a

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 <link rel="stylesheet" href="/w3.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="/font-awesome.min.css">
 	<head>
   		<title>ATN company - HOMEPAGE</title>
  	</head>
