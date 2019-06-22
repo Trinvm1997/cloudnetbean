3c820f4b04fcd09ed8b4902048f17cc4c8f8d85c
nguyen tri <rinthu1997@gmail.com>
2019-06-22 12:00:42 +0700

    edit

--- DeleteData.php
+++ DeleteData.php
@@ -4,31 +4,37 @@
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<head>
-		<title>ATN - Deleting</title>
-	</head>
 
-	<body>
-		<div class="w3-container">
-			<div class="w3-bar w3-black w3-card">
-				<a href="index.php" class="w3-bar-item w3-button w3-padding-large w3-hover-text-blue">HOME</a>
-				<a href="ConnectToDB.php" class="w3-bar-item w3-button w3-padding-large w3-right w3-hover-text-blue">VIEW</a>
-    			<a href="InsertData.php" class="w3-bar-item w3-button w3-padding-large w3-right w3-hover-text-blue">INSERT</a>
-    			<a href="UpdateData.php" class="w3-bar-item w3-button w3-padding-large w3-right w3-hover-text-blue">UPDATE</a>
-    			<a href="DeleteData.php" class="w3-bar-item w3-button w3-padding-large w3-right w3-hover-text-blue">DELETE</a>
-			</div>
+<head>
+	<title>ATN - Deleting</title>
+</head>
+
+<body>
+	<div class="w3-container">
+		<div class="w3-bar w3-black w3-card">
+			<a href="index.php" class="w3-bar-item w3-button w3-padding-large w3-hover-text-blue">HOME</a>
+			<a href="ConnectToDB.php"
+				class="w3-bar-item w3-button w3-padding-large w3-right w3-hover-text-blue">VIEW</a>
+			<a href="InsertData.php"
+				class="w3-bar-item w3-button w3-padding-large w3-right w3-hover-text-blue">INSERT</a>
+			<a href="UpdateData.php"
+				class="w3-bar-item w3-button w3-padding-large w3-right w3-hover-text-blue">UPDATE</a>
+			<a href="DeleteData.php"
+				class="w3-bar-item w3-button w3-padding-large w3-right w3-hover-text-blue">DELETE</a>
 		</div>
+	</div>
 
-		<div class="w3-container">
-			<h1>DELETE DATA FROM DATABASE</h1>
-			<ul style="list-style-type: none">
-			<form name="DeleteData" action="DeleteData.php" method="POST" >
-			<li>Shop ID:</li><li><input type="text" name="shopid" /></li>
-			<li><input type="submit" value="DELETE" /></li>
+	<div class="w3-container">
+		<h1>DELETE DATA FROM DATABASE</h1>
+		<ul style="list-style-type: none">
+			<form name="DeleteData" action="DeleteData.php" method="POST">
+				<li>Shop ID:</li>
+				<li><input type="text" name="shopid" /></li>
+				<li><input type="submit" value="DELETE" /></li>
 			</form>
-			</ul>
+		</ul>
 
-			<?php
+		<?php
 			if (empty(getenv("DATABASE_URL"))){
 				echo '<p>The DB does not exist</p>';
 				$pdo = new PDO('pgsql:host=localhost;port=5432;dbname=mydb', 'postgres', '123456');
@@ -55,16 +61,18 @@
 				}
 			}
 			?>
-		</div>
+	</div>
 
-		<footer class="w3-container w3-padding w3-center w3-black w3-xlarge">
-			<i class="fa fa-facebook-official w3-hover-opacity"></i>
-			<i class="fa fa-instagram w3-hover-opacity"></i>
-			<i class="fa fa-snapchat w3-hover-opacity"></i>
-			<i class="fa fa-pinterest-p w3-hover-opacity"></i>
-			<i class="fa fa-twitter w3-hover-opacity"></i>
-			<i class="fa fa-linkedin w3-hover-opacity"></i>
-			<p class="w3-medium w3-text-white">Designed by <a href="https://www.facebook.com/profile.php?id=100005135791217" target="_blank">Nguyen Van Minh Tri</a></p>
-		</footer>
-	</body>
+	<footer class="w3-container w3-padding w3-center w3-black w3-xlarge">
+		<i class="fa fa-facebook-official w3-hover-opacity"></i>
+		<i class="fa fa-instagram w3-hover-opacity"></i>
+		<i class="fa fa-snapchat w3-hover-opacity"></i>
+		<i class="fa fa-pinterest-p w3-hover-opacity"></i>
+		<i class="fa fa-twitter w3-hover-opacity"></i>
+		<i class="fa fa-linkedin w3-hover-opacity"></i>
+		<p class="w3-medium w3-text-white">Designed by <a href="https://www.facebook.com/profile.php?id=100005135791217"
+				target="_blank">Nguyen Van Minh Tri</a></p>
+	</footer>
+</body>
+
 </html>
--- index.php
+++ index.php
@@ -3,33 +3,41 @@
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<head>
-		<title>ATN - Homepage</title>
-	</head>
 
-	<body>
-		<div class="w3-container">
-			<div class="w3-bar w3-black w3-card">
-				<a href="index.php" class="w3-bar-item w3-button w3-padding-large w3-hover-text-blue">HOME</a>
-				<a href="ConnectToDB.php" class="w3-bar-item w3-button w3-padding-large w3-right w3-hover-text-blue">VIEW</a>
-    			<a href="InsertData.php" class="w3-bar-item w3-button w3-padding-large w3-right w3-hover-text-blue">INSERT</a>
-    			<a href="UpdateData.php" class="w3-bar-item w3-button w3-padding-large w3-right w3-hover-text-blue">UPDATE</a>
-    			<a href="DeleteData.php" class="w3-bar-item w3-button w3-padding-large w3-right w3-hover-text-blue">DELETE</a>
-			</div>
+<head>
+	<title>ATN - Homepage</title>
+</head>
+
+<body>
+	<div class="w3-container">
+		<div class="w3-bar w3-black w3-card">
+			<a href="index.php" class="w3-bar-item w3-button w3-padding-large w3-hover-text-blue">HOME</a>
+			<a href="ConnectToDB.php"
+				class="w3-bar-item w3-button w3-padding-large w3-right w3-hover-text-blue">VIEW</a>
+			<a href="InsertData.php"
+				class="w3-bar-item w3-button w3-padding-large w3-right w3-hover-text-blue">INSERT</a>
+			<a href="UpdateData.php"
+				class="w3-bar-item w3-button w3-padding-large w3-right w3-hover-text-blue">UPDATE</a>
+			<a href="DeleteData.php"
+				class="w3-bar-item w3-button w3-padding-large w3-right w3-hover-text-blue">DELETE</a>
 		</div>
+	</div>
 
-		<div class="w3-container">
-			<img  style="width:100%; height:auto; margin:0 auto" src="https://cdn.freebiesupply.com/logos/large/2x/atn-2-logo-png-transparent.png" alt="logo">
-		</div>
+	<div class="w3-container">
+		<img style="width:100%; height:auto; margin:0 auto"
+			src="https://cdn.freebiesupply.com/logos/large/2x/atn-2-logo-png-transparent.png" alt="logo">
+	</div>
 
-		<footer class="w3-container w3-padding w3-center w3-black w3-xlarge">
-			<i class="fa fa-facebook-official w3-hover-opacity"></i>
-			<i class="fa fa-instagram w3-hover-opacity"></i>
-			<i class="fa fa-snapchat w3-hover-opacity"></i>
-			<i class="fa fa-pinterest-p w3-hover-opacity"></i>
-			<i class="fa fa-twitter w3-hover-opacity"></i>
-			<i class="fa fa-linkedin w3-hover-opacity"></i>
-			<p class="w3-medium w3-text-white">Designed by <a href="https://www.facebook.com/profile.php?id=100005135791217" target="_blank">Nguyen Van Minh Tri</a></p>
-		</footer>
-	</body>
+	<footer class="w3-container w3-padding w3-center w3-black w3-xlarge">
+		<i class="fa fa-facebook-official w3-hover-opacity"></i>
+		<i class="fa fa-instagram w3-hover-opacity"></i>
+		<i class="fa fa-snapchat w3-hover-opacity"></i>
+		<i class="fa fa-pinterest-p w3-hover-opacity"></i>
+		<i class="fa fa-twitter w3-hover-opacity"></i>
+		<i class="fa fa-linkedin w3-hover-opacity"></i>
+		<p class="w3-medium w3-text-white">Designed by <a href="https://www.facebook.com/profile.php?id=100005135791217"
+				target="_blank">Nguyen Van Minh Tri</a></p>
+	</footer>
+</body>
+
 </html>
